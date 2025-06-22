8bd54238cbd4a920a4928004246a68272151dd75
Yuliya <yu.v.barkova@yandex.ru>
2025-06-22 14:04:57 +0700

    2 commit

--- src/test/java/ru/netology/service/FreeLancerServiceTest.java
+++ src/test/java/ru/netology/service/FreeLancerServiceTest.java
@@ -2,6 +2,8 @@ package ru.netology.service;
 
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -30,6 +32,18 @@ class FreeLancerServiceTest {
         int expected = 2;
         Assertions.assertEquals(expected, actual);
 
+    }
+
+    @ParameterizedTest
+    @CsvFileSource (resources = "test_data.csv")
+    public void testWithParams(int income, int expences, int threshold, int expected) {
+
+        FreeLancerService service = new FreeLancerService();
+
+        int actual = service.calculate(income, expences, threshold);
+
+
+        Assertions.assertEquals(expected, actual);
     }
 }
 
